37913273eeeccd100aa892f6d12a2c8c04025dbf
Sam <samwrigley01@gmail.com>
2020-09-18 17:10:36 +1000

    some audio added

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource1.PlayScheduled(AudioSettings.dspTime);
+        double clipLength = audioSource1.clip.samples / audioSource1.clip.frequency;
+        audioSource2.PlayScheduled(AudioSettings.dspTime + clipLength);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
