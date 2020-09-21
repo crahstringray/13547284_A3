c060d889ed35eaac5f2325f43c4bd0faf79dd509
Sam <samwrigley01@gmail.com>
2020-09-21 20:04:53 +1000

    Tween started

---
+++ Assets/Scripts/PacManMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManMover : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    private Tweener tweener;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tweener.AddTween(item.transform, item.transform.position, new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
+       
+    }
+}
